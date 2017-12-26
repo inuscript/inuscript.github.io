9eb6e7c5eb2f9b64adf09e18faa02d67957ab301
inuscript <inuscript@users.noreply.github.com>
2017-12-26 23:38:18 +0900

    add dev-to

--- src/Links.js
+++ src/Links.js
@@ -32,11 +32,14 @@ export const Links = () => {
     <List href="https://www.npmjs.com/~inuscript" oldHref="https://www.npmjs.com/~suisho">
       npm
     </List>
-    <List href="https://twitter.com/inuscript">
-      Twitter
-    </List>
     <List href="https://qiita.com/inuscript">
       Qiita
+    </List>
+    <List href="https://dev.to/inuscript">
+      dev.to
+    </List>
+    <List href="https://twitter.com/inuscript">
+      Twitter
     </List>
     <List href="http://sui.hateblo.jp/">
       Blog
