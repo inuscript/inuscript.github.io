21c8794b8e9aa39f9ff876ba189363b6e0d65994
inuscript <inuscript@users.noreply.github.com>
2017-12-26 23:40:42 +0900

    add medium

--- src/Links.js
+++ src/Links.js
@@ -35,6 +35,9 @@ export const Links = () => {
     <List href="https://qiita.com/inuscript">
       Qiita
     </List>
+    <List href="https://medium.com/inuscript/">
+      Medium
+    </List>
     <List href="https://dev.to/inuscript">
       dev.to
     </List>
