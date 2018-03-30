6c59404aba11ca85a2c76b5131c0ee9907d633b3
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-03-30 19:01:24 +0900

    data-netlify

--- src/form/Form.js
+++ src/form/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Form = () => {
   return (
-    <form method="POST" netlify>
+    <form method="POST" data-netlify="true">
       <input name="text" />
       <button type="submit">Send</button>
     </form>
