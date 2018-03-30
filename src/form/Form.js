93191210e2e54d7e53d50e460dc06364d3d5b11f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-03-30 19:07:36 +0900

    name contact

--- src/form/Form.js
+++ src/form/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Form = () => {
   return (
-    <form method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true">
       <div>
         お名前: <input name="text" />
       </div>
