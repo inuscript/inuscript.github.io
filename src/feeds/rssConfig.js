407bdf1aaa1e62a3616852754eaf4f46767acc1e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-03-02 22:26:49 +0900

    remove dev.to field

--- src/feeds/rssConfig.js
+++ src/feeds/rssConfig.js
@@ -5,12 +5,12 @@ export default [
     dev: null,
     bgColor: "#55c500"
   },
-  {
-    media: "dev.to",
-    production: "/feed/devto",
-    dev: "https://dev.to/feed/terrierscript",
-    bgColor: "#000"
-  },
+  // {
+  //   media: "dev.to",
+  //   production: "/feed/devto",
+  //   dev: "https://dev.to/feed/terrierscript",
+  //   bgColor: "#000"
+  // },
   {
     media: "Medium",
     production: "/feed/medium",
